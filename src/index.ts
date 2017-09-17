b2bfe1d28fd70ef2260df8da48e0825b19ce7fba
stoiet <to.stoiet@gmail.com>
2017-09-17 17:11:56 +0200

    feat(server): add body parser

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import './source-map-support';
 import { createServer as createHTTPSServer } from 'https';
 import { createServer as createHTTPServer } from 'http';
 import { config } from './config';
+
+import * as bodyParser from 'koa-bodyparser';
 import * as Koa from 'koa';
 
 const application = new Koa();
@@ -10,7 +12,18 @@ application.env = config.NODE_ENV;
 application.proxy = false;
 application.silent = false;
 
+application.use(bodyParser({
+  enableTypes: ['json'],
+  encode: 'utf-8',
+  jsonLimit: '100kb',
+  strict: true,
+  onerror: (error, context) => {
+    return context.throw(400, 'Error during body parse!', { message: error.message });
+  }
+}));
+
 application.use(async ({ response }) => {
+  console.info('Request arrived!');
   response.body = { success: true };
   response.status = 200;
 });
